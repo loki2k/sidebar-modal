d70b8c0043480abd13292aa7f0500c1a7a58d734
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:54:51 -0800

    Updates

---
+++ precache-manifest.891c923d98f526fcc423486bc16ce721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2bdc6c4e03f23429953822a07eea40",
+    "url": "/sidebar-modal/index.html"
+  },
+  {
+    "revision": "9ecf335cdc027b50f1bc",
+    "url": "/sidebar-modal/static/css/main.19d3afdf.chunk.css"
+  },
+  {
+    "revision": "00dabe24ad094e2a245a",
+    "url": "/sidebar-modal/static/js/2.43f1e553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sidebar-modal/static/js/2.43f1e553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ecf335cdc027b50f1bc",
+    "url": "/sidebar-modal/static/js/main.aafb536c.chunk.js"
+  },
+  {
+    "revision": "a8cd427f675fc95d7c22",
+    "url": "/sidebar-modal/static/js/runtime-main.328f120b.js"
+  },
+  {
+    "revision": "2b686f2e5682570fb85fc952a4581419",
+    "url": "/sidebar-modal/static/media/logo.2b686f2e.svg"
+  }
+]);
